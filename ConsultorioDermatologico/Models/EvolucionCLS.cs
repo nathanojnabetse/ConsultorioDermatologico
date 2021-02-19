cfead740fda2934ec965a2184e6d9edb71cbcbd1
Jona <nathanojnabetse@hotmail.com>
2021-02-19 01:56:29 -0500

    Corrección ortógrafica de advertencia
    Correción del texto Longitud Mmaxima -> Longitud Máxima

--- ConsultorioDermatologico/Models/EvolucionCLS.cs
+++ ConsultorioDermatologico/Models/EvolucionCLS.cs
@@ -13,27 +13,27 @@ namespace ConsultorioDermatologico.Models
         public string mapaCorporal { get; set; } //recuperar la cadena
         [Required]
         [DataType(DataType.MultilineText)]
-        [StringLength(5000, ErrorMessage = "Longitud Mmaxima 5000")]
+        [StringLength(5000, ErrorMessage = "Longitud Máxima 5000")]
         [Display(Name = "Diagnóstico")]
         public string diagnostico { get; set; }
         [Required]
         [DataType(DataType.MultilineText)]
-        [StringLength(5000, ErrorMessage = "Longitud Mmaxima 5000")]
+        [StringLength(5000, ErrorMessage = "Longitud Máxima 5000")]
         [Display(Name = "Motivo de consulta")]
         public string motivoConsulta { get; set; }
         [Required]
         [DataType(DataType.MultilineText)]
-        [StringLength(5000, ErrorMessage = "Longitud Mmaxima 5000")]
+        [StringLength(5000, ErrorMessage = "Longitud Máxima 5000")]
         [Display(Name = "Exámen físico")]
         public string examenFisico { get; set; }
         [Required]
         [DataType(DataType.MultilineText)]
-        [StringLength(5000, ErrorMessage = "Longitud Mmaxima 5000")]
+        [StringLength(5000, ErrorMessage = "Longitud Máxima 5000")]
         [Display(Name = "Prescripción")]
         public string prescripcion { get; set; }
         [Required]
         [DataType(DataType.MultilineText)]
-        [StringLength(5000, ErrorMessage = "Longitud Mmaxima 5000")]
+        [StringLength(5000, ErrorMessage = "Longitud Máxima 5000")]
         [Display(Name = "Recomendaciones")]
         public string recomendaciones { get; set; }
         public DateTime fechaVisita { get; set; }
