b74cdc659b313b7cd0483a205b84732371dc9cdf
Jona <nathanojnabetse@hotmail.com>
2021-02-27 22:07:25 -0500

    Implementados los metodos para borrar y restablecer usuarios y visitas
    El administrados puede eliminar definitivamente o restablecer usuarios y visitas deshabilitadas por un usuario medico.
    
    Corrección de sintaxis en el uso de Jquery en /Views/Usuario/Index.cshtml en eliminar.

--- ConsultorioDermatologico/Controllers/AdministracionPacientesController.cs
+++ ConsultorioDermatologico/Controllers/AdministracionPacientesController.cs
@@ -139,5 +139,141 @@ namespace ConsultorioDermatologico.Controllers
 
             return PartialView("_TablaDesactivados");
         }
+
+        public int EliminarPaciente(int? idPaciente)
+        {
+            int rpta = 0;
+            try
+            {
+                using (var bd = new BDD_ConsultorioDermatologicoEntities())
+                {
+                    if(idPaciente!=null)
+                    {
+                        tblPaciente tblPaciente = bd.tblPaciente.Where(p => p.idPaciente == idPaciente).First();
+                        tblHistoriaClinica tblHistoriaClinica = bd.tblHistoriaClinica.Where(p => p.idPaciente == tblPaciente.idPaciente).First();
+                        List<tblEvolucion> listaEvoluciones = bd.tblEvolucion.Where(p => p.idHistoriaClinica == tblHistoriaClinica.idHistoriaClinica).ToList();
+                        List<tblFotos> listaFotos = new List<tblFotos>();
+                        foreach (var item in listaEvoluciones)
+                        {
+                            List<tblFotos> tblFotos = bd.tblFotos.Where(p => p.idEvolucion == item.idEvolucion).ToList();
+                            foreach (var item2 in tblFotos)
+                            {
+                                bd.tblFotos.Remove(item2);
+                            }
+                            bd.tblEvolucion.Remove(item);
+                        }
+                        bd.tblHistoriaClinica.Remove(tblHistoriaClinica);
+                        bd.tblPaciente.Remove(tblPaciente);
+
+                        rpta = bd.SaveChanges();
+                    }
+                    else
+                    {
+                        rpta = 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                rpta = 0;
+            }
+            return rpta;
+        }
+        public int ReestablecerPaciente(int? idPaciente)
+        {
+            int rpta = 0;
+            try
+            {
+                using (var bd = new BDD_ConsultorioDermatologicoEntities())
+                {
+                    if (idPaciente != null)
+                    {
+                        tblPaciente tblPaciente = bd.tblPaciente.Where(p => p.idPaciente == idPaciente).First();
+                        tblHistoriaClinica tblHistoriaClinica = bd.tblHistoriaClinica.Where(p => p.idPaciente == tblPaciente.idPaciente).First();
+                        List<tblEvolucion> listaEvoluciones = bd.tblEvolucion.Where(p => p.idHistoriaClinica == tblHistoriaClinica.idHistoriaClinica && p.habilitado == 0).ToList();
+                        foreach (var item in listaEvoluciones)
+                        {
+                            item.habilitado = 1;
+                        }
+                        tblHistoriaClinica.habilitado = 1;
+                        tblPaciente.habilitado = 1;
+
+                        rpta = bd.SaveChanges();
+                    }
+                    else
+                    {
+                        rpta = 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                rpta = 0;
+            }
+            return rpta;
+        }
+
+        public int EliminarVisita(int? idEvolucion)
+        {
+            int rpta = 0;
+            try
+            {
+                using (var bd = new BDD_ConsultorioDermatologicoEntities())
+                {
+                    if (idEvolucion != null)
+                    {
+                        tblEvolucion tblEvolucion = bd.tblEvolucion.Where(p => p.idEvolucion == idEvolucion).First();
+                        List<tblFotos> listaFotos = bd.tblFotos.Where(p => p.idEvolucion == tblEvolucion.idEvolucion).ToList();
+                        foreach(var item in listaFotos)
+                        {
+                            bd.tblFotos.Remove(item);
+                        }                      
+                        bd.tblEvolucion.Remove(tblEvolucion);
+
+                        rpta = bd.SaveChanges();
+                    }
+                    else
+                    {
+                        rpta = 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                rpta = 0;
+            }
+            return rpta;
+        }
+
+        public int ReestablecerVisita(int? idEvolucion)
+        {
+            int rpta = 0;
+            try
+            {
+                using (var bd = new BDD_ConsultorioDermatologicoEntities())
+                {
+                    if (idEvolucion != null)
+                    {
+                        tblEvolucion tblEvolucion = bd.tblEvolucion.Where(p => p.idEvolucion == idEvolucion).First();
+                        tblEvolucion.habilitado = 1;
+
+                        rpta = bd.SaveChanges();
+                    }
+                    else
+                    {
+                        rpta = 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                rpta = 0;
+            }
+            return rpta;
+        }
     }
-}
+}   
