03e97156e0b2ed29ef78a56170bde7099bb773ba
Jona <nathanojnabetse@hotmail.com>
2021-03-01 08:48:09 -0500

    Creación de la vista de login

--- ConsultorioDermatologico/App_Start/RouteConfig.cs
+++ ConsultorioDermatologico/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ConsultorioDermatologico
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Paciente", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
