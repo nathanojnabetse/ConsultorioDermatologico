6b910d2806b51edffa7cfa289348bc42cc8581ff
Jona <nathanojnabetse@hotmail.com>
2021-03-01 20:09:51 -0500

    Implementacion de una Custom Error Page
    Manejo de errores 404,505 y desconocidos.
    Correción de estilo en el login.

---
+++ ConsultorioDermatologico/Controllers/ErrorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsultorioDermatologico.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index(int error = 0)
+        {
+            switch (error)
+            {
+                case 505:
+                    ViewBag.Title = "Ocurrio un error inesperado";
+                    ViewBag.Description = "Esperemos que esto no vuelva a suceder...";
+                    break;
+
+                case 404:
+                    ViewBag.Title = "Página no encontrada";
+                    ViewBag.Description = "La URL al que está intentando ingresar no existe";
+                    break;
+
+                default:
+                    ViewBag.Title = "Página no encontrada";
+                    ViewBag.Description = "Algo salio ha salido mal :(";
+                    break;
+            }
+
+            return View("~/Views/Error/_ErrorPage.cshtml");
+        }
+    }
+}
